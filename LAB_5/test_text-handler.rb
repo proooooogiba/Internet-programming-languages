b9ee7bbb9c6d83daffee8b09a2aefccd25bc0998
Ivan Pogiba <pogibuskaivan@gmail.com>
2022-10-01 22:46:26 +0300

    Labaratory 5 partly complete

--- LAB_5/test_text-handler.rb
+++ LAB_5/test_text-handler.rb
@@ -1,7 +1,18 @@
 require "text-handler.rb"
+require './text-handler.rb'
+
 require "minitest/unit"
 
 class TestTextHandler < MiniTest::Unit::TestCase
     def setup
-        @text_handler = TextHandler.new("Hello world")
+        @text_handler = TextHandler.new
+        @text_handler.handle();
     end
+
+    def test_1
+    end    
+    
+    
+    def teardown
+    end
+end
