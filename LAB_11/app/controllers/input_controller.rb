ddfb37313747c402155d5976d7aa244329d4dc70
Ivan Pogiba <pogibuskaivan@gmail.com>
2022-12-22 12:28:21 +0300

    LAB_11 ia added correctly

--- LAB_11/app/controllers/input_controller.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class InputController < ApplicationController
-  def view; end
-end
--- LAB_11/app/helpers/calc_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module CalcHelper
-  def automorf(n)
-    arr = []
-    (1..n).each { |i| arr.append(i) if (i**2).to_s.end_with?(i.to_s) }
-    arr
-  end
-end
