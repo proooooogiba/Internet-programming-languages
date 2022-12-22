aca6fb83d3664822ce19c5ba7f34b858891968ff
Погиба Иван <pogibuskaivan@gmail.com>
2022-12-22 12:56:35 +0300

    Delete LAB_5_NEW directory

--- LAB_5_NEW/part-1.rb
+++
@@ -1,7 +0,0 @@
-print 'Input x to find f(x, y): '
-x = Integer(gets.chomp)
-print 'Input y to find f(x, y): '
-y = Integer(gets.chomp)
-
-string = "f(#{x}, #{y}): "
-puts (((x - 1).abs**(1.0 / 2) - (y).abs**(1.0 / 2)) / (1 + x**2/2 + y**2/4))
